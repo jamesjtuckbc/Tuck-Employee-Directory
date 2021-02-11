cc40fa9555ceca33ff5b5451e426be1285025ffb
James Tuck <JamesjTuck@gmail.com>
2021-02-10 23:18:50 -0700

    Updates

--- tuck-employee-directory/src/components/Search/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Search(props) {
-    return (
-        <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" onChange={props.handleSearch} />
-    )
-}
-
-export default Search;
